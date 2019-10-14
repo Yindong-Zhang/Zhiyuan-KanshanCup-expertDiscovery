0d8525c04aeb5e5037e4a0caa2c1138b51e6288c
yindong <so_go_flying@163.com>
2019-10-14 19:18:43 +0800

    1. BiLSTMRCNN test conclude.

--- ydzhang/src/layers.py
+++ ydzhang/src/layers.py
@@ -1,24 +1,30 @@
 from torch import nn
 import torch
-import torch.functional as F
+import torch.nn.functional as F
 
 
 class BiLSTMRCNN(nn.Module):
-    def __init__(self, word_embeddings):
+    def __init__(self,
+                 embedding_size,
+                 hidden_dim,
+                 hidden_layers,
+                 word_embeddings,
+                 embedding_dropout= 0.1,
+                 rnn_dropout = 0.1,
+                 ):
         super(BiLSTMRCNN, self).__init__()
 
-        self.embed_size = 200
-        self.label_num = 10
-        self.embed_dropout = 0.1
-        self.fc_dropout = 0.1
-        self.hidden_num = 2
-        self.hidden_size = 50
-        self.hidden_dropout = 0
+        self.embed_size = embedding_size
+        self.hidden_num = hidden_layers
+        self.hidden_size = hidden_dim
+        self.hidden_dropout = rnn_dropout
+        self.embedding_dropout_rate = embedding_dropout
         self.bidirectional = True
 
-        self.embeddings = nn.Embedding(len(word_embeddings),self.embed_size)
-        self.embeddings.weight.data.copy_(torch.from_numpy(word_embeddings))
-        self.embeddings.weight.requires_grad = False
+        self.embedding_layer = nn.Embedding(len(word_embeddings), self.embed_size)
+        self.embedding_layer.weight.data.copy_(torch.from_numpy(word_embeddings))
+        self.embedding_layer.weight.requires_grad = False
+        self.embedding_dropout_layer = nn.Dropout(self.embedding_dropout_rate)
 
         self.lstm = nn.LSTM(
             self.embed_size,
@@ -26,31 +32,23 @@ class BiLSTMRCNN(nn.Module):
             dropout=self.hidden_dropout,
             num_layers=self.hidden_num,
             batch_first=True,
-            bidirectional=self.bidirectional
+            bidirectional= True,
         )
-        self.embed_dropout = nn.Dropout(self.embed_dropout)
-        self.fc_dropout = nn.Dropout(self.fc_dropout)
-        self.linear1 = nn.Linear(self.hidden_size * 2, self.hidden_size // 2)
-        self.linear2 = nn.Linear(self.hidden_size // 2, self.label_num)
 
     def forward(self, input):
-        out = self.embeddings(input)
-        out = self.embed_dropout(out)
+        out = self.embedding_layer(input)
+        out = self.embedding_dropout_layer(out)
         out, _ = self.lstm(out)
 
         out = torch.transpose(out, 1, 2)
 
         out = torch.tanh(out)
 
-        out = F.max_pool1d(out, out.size(2))
+        out = F.max_pool1d(out, out.size(2), )
 
         out = out.squeeze(2)
 
-        out = self.fc_dropout(out)
-        out = self.linear1(F.relu(out))
-        output = self.linear2(F.relu(out))
-
-        return output
+        return out
 
 
 class FullyConnectedLayer(nn.Module):
@@ -90,3 +88,12 @@ class FullyConnectedLayer(nn.Module):
 
     def forward(self, x):
         return self.output_layer(self.fc(x)) if self.sigmoid else self.fc(x)
+
+
+if __name__ == '__main__':
+    import numpy as np
+    seq = torch.randint(0, 256, (3, 32))
+    word_embeddings = np.random.randn(256, 128)
+    t = BiLSTMRCNN(embedding_size= 128, hidden_dim= 256, hidden_layers= 3, word_embeddings= word_embeddings)
+    out = t(seq)
+    print(out.detach().numpy())
